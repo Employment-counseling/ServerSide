7aa8238df0fc684befdfea07d3b5d1bf8acd53d2
RacheliKrol <r0527120682@gmail.com>
2024-12-09 10:16:59 +0200

    Add project files.

---
+++ Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploymentCounseling.Entities
+{
+  
+	enum Payment
+	{
+		NotPaid=1,
+		PartiallyPaid = 2,
+        Paid = 3
+	}
+	internal class Client
+	{
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public PackageName Package { get; set; }
+        public Payment IsPaid { get; set; }
+        public bool IsAnswered {get; set; }
+
+    }
+}
---
+++ Entities/Questionnaire.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace EmploymentCounseling.Entities
+{
+	public enum PackageName
+	{
+		basic = 1,
+		medium = 2,
+		full = 3,
+		primium = 4
+	}
+	public class QuestionItem
+	{
+        public string Question { get; set; }
+        public dynamic Answer { get; set; }
+    }
+
+    public class Questionnaire
+    {
+        public string Name { get; set; }
+		List<PackageName> Packages { get; set; }
+		List<QuestionItem> Questions { get; set; }
+
+	}
+
+	public class Package
+	{
+        public Guid Id { get; set; }
+        public PackageName Name { get; set; }
+        public int Price { get; set; }
+        public List<Questionnaire> Questionnaires { get; set; }
+    }
+
+
+
+
+}
