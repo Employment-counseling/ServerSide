586db610166d6c67c91cd92b8666279728998f62
RacheliKrol <r0527120682@gmail.com>
2025-06-11 14:46:13 +0300

    Add migration for the new model

---
+++ Employment-Counseling/Migrations/20250611112023_InitialCreate.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employment_Counseling.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Packages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    IncludeCall = table.Column<bool>(type: "bit", nullable: false),
+                    IncludeTilDiagnosis = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questionnaires",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questionnaires", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCostumer = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackageQuestionnaire",
+                columns: table => new
+                {
+                    PackagesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionnairesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackageQuestionnaire", x => new { x.PackagesId, x.QuestionnairesId });
+                    table.ForeignKey(
+                        name: "FK_PackageQuestionnaire_Packages_PackagesId",
+                        column: x => x.PackagesId,
+                        principalTable: "Packages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PackageQuestionnaire_Questionnaires_QuestionnairesId",
+                        column: x => x.QuestionnairesId,
+                        principalTable: "Questionnaires",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    OptionsRaw = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionnaireId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionItems_Questionnaires_QuestionnaireId",
+                        column: x => x.QuestionnaireId,
+                        principalTable: "Questionnaires",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Costumers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsPaid = table.Column<int>(type: "int", nullable: false),
+                    IsComplitedQuestionnaires = table.Column<bool>(type: "bit", nullable: false),
+                    IsAnswered = table.Column<bool>(type: "bit", nullable: false),
+                    PackageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Costumers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Costumers_Packages_PackageId",
+                        column: x => x.PackageId,
+                        principalTable: "Packages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Costumers_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Counselors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Discription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Counselors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Counselors_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CostumerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionnaireId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateSubmitted = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAnswers_Costumers_CostumerId",
+                        column: x => x.CostumerId,
+                        principalTable: "Costumers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserAnswers_Questionnaires_QuestionnaireId",
+                        column: x => x.QuestionnaireId,
+                        principalTable: "Questionnaires",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CounselorPackage",
+                columns: table => new
+                {
+                    CounselorsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PackagesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CounselorPackage", x => new { x.CounselorsId, x.PackagesId });
+                    table.ForeignKey(
+                        name: "FK_CounselorPackage_Counselors_CounselorsId",
+                        column: x => x.CounselorsId,
+                        principalTable: "Counselors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CounselorPackage_Packages_PackagesId",
+                        column: x => x.PackagesId,
+                        principalTable: "Packages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnswerItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserAnswersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnswerItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnswerItems_QuestionItems_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "QuestionItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AnswerItems_UserAnswers_UserAnswersId",
+                        column: x => x.UserAnswersId,
+                        principalTable: "UserAnswers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnswerItems_QuestionId",
+                table: "AnswerItems",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnswerItems_UserAnswersId",
+                table: "AnswerItems",
+                column: "UserAnswersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Costumers_PackageId",
+                table: "Costumers",
+                column: "PackageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CounselorPackage_PackagesId",
+                table: "CounselorPackage",
+                column: "PackagesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackageQuestionnaire_QuestionnairesId",
+                table: "PackageQuestionnaire",
+                column: "QuestionnairesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionItems_QuestionnaireId",
+                table: "QuestionItems",
+                column: "QuestionnaireId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAnswers_CostumerId",
+                table: "UserAnswers",
+                column: "CostumerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAnswers_QuestionnaireId",
+                table: "UserAnswers",
+                column: "QuestionnaireId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnswerItems");
+
+            migrationBuilder.DropTable(
+                name: "CounselorPackage");
+
+            migrationBuilder.DropTable(
+                name: "PackageQuestionnaire");
+
+            migrationBuilder.DropTable(
+                name: "QuestionItems");
+
+            migrationBuilder.DropTable(
+                name: "UserAnswers");
+
+            migrationBuilder.DropTable(
+                name: "Counselors");
+
+            migrationBuilder.DropTable(
+                name: "Costumers");
+
+            migrationBuilder.DropTable(
+                name: "Questionnaires");
+
+            migrationBuilder.DropTable(
+                name: "Packages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
